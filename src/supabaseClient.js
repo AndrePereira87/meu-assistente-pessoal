472729640ba819dcc57a5d011d6142299e688040
AndrePereira87 <al81393@alunos.utad.pt>
2026-06-19 10:40:17 +0100

    chore: mover chaves de api para ficheiro .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Vais buscar estes dois valores ao teu painel do Supabase
-// (Settings -> API -> Project URL & anon public key)
-const supabaseUrl = 'https://qstqfivkgmyhqnocacxd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzdHFmaXZrZ215aHFub2NhY3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNjMyOTksImV4cCI6MjA5NDkzOTI5OX0.Yasi5-Fqcmyi0WAEmKxI9rsXmRyoFxFKoXPgMV_1A_I'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
